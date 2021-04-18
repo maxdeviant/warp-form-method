c54823ce14ccb95594883ac5cc2f19e6953f5342
Marshall Bowers <elliott.codes@gmail.com>
2021-04-18 13:31:19 -0400

    Add badges

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # warp_form_method
 
-A warp filter for accepting form submissions using any HTTP method.
+[![Crates.io](https://img.shields.io/crates/v/warp_form_method.svg)](https://crates.io/crates/warp_form_method)
+[![Docs.rs](https://docs.rs/warp_form_method/badge.svg)](https://docs.rs/warp_form_method/)
+[![Crates.io](https://img.shields.io/crates/l/warp_form_method.svg)](https://github.com/maxdeviant/warp-form-method/blob/master/LICENSE)
+
+A [warp](https://github.com/seanmonstar/warp) filter for accepting form submissions using any HTTP method.
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
 //! A warp filter for accepting form submissions using any HTTP method.
+//!
+//! [![Crates.io](https://img.shields.io/crates/v/warp_form_method.svg)](https://crates.io/crates/warp_form_method)
+//! [![Docs.rs](https://docs.rs/warp_form_method/badge.svg)](https://docs.rs/warp_form_method/)
+//! [![Crates.io](https://img.shields.io/crates/l/warp_form_method.svg)](https://github.com/maxdeviant/warp-form-method/blob/master/LICENSE)
+//!
+//! ## Installation
+//! ```toml
+//! [dependencies]
+//! warp_form_method = "0.1"
+//! ```
 
 #![warn(missing_docs)]
 
